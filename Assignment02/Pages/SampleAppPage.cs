4c67ce5b6a4d69f9f04221188ce2c826e7df058c
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-02-06 12:38:27 +0530

    Add the Sample app page & test file

---
+++ Assignment02/Pages/SampleAppPage.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CSE2522_Assignment02.Pages
+{
+    public class SampleAppPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public SampleAppPage(IWebDriver driver, WebDriverWait wait)
+        {
+            this.driver = driver;
+            this.wait = wait;
+        }
+
+      
+        private IWebElement Username =>
+            wait.Until(d => d.FindElement(By.Name("UserName")));
+
+        private IWebElement Password =>
+            wait.Until(d => d.FindElement(By.Name("Password")));
+
+        private IWebElement LoginButton =>
+            wait.Until(d => d.FindElement(By.Id("login")));
+
+        private IWebElement StatusMessage =>
+            wait.Until(d => d.FindElement(By.Id("loginstatus")));
+
+        
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://uitestingplayground.com/sampleapp");
+        }
+
+        
+        public bool IsPageLoaded()
+        {
+            return Username.Displayed
+                && Password.Displayed
+                && LoginButton.Displayed;
+        }
+
+        
+        public void Login(string username, string password)
+        {
+            Username.Clear();
+            Username.SendKeys(username);
+
+            Password.Clear();
+            Password.SendKeys(password);
+
+            LoginButton.Click();
+        }
+
+        
+        public string GetStatus()
+        {
+            return StatusMessage.Text;
+        }
+    }
+}
---
+++ Assignment02/Tests/SampleAppTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using CSE2522_Assignment02.Base;
+using CSE2522_Assignment02.Pages;
+
+namespace CSE2522_Assignment02.Tests
+{
+    public class SampleAppTests : BaseTest
+    {
+        // TC002-[1] – Page verification
+        [Test]
+        [Description("TC002-[1] - Sample app page verification")]
+        public void VerifySampleAppPage()
+        {
+            var page = new SampleAppPage(driver, wait);
+            page.Open();
+
+            Assert.That(page.IsPageLoaded(), Is.True);
+        }
+
+        // TC002-[2] – Successful login
+        [Test]
+        [Description("TC002-[2] - Successful login")]
+        public void VerifySuccessfulLogin()
+        {
+            var page = new SampleAppPage(driver, wait);
+            page.Open();
+            page.Login("test", "pwd");
+
+            Assert.That(page.GetStatus(), Does.Contain("Welcome"));
+        }
+
+        // TC002-[3] – Unsuccessful login
+        [Test]
+        [Description("TC002-[3] - Unsuccessful login")]
+        public void VerifyUnsuccessfulLogin()
+        {
+            var page = new SampleAppPage(driver, wait);
+            page.Open();
+            page.Login("test", "wrong");
+
+            Assert.That(page.GetStatus(), Does.Contain("Invalid"));
+        }
+    }
+}
