5feed190798e0cbc00e0b34ad9d35bd4e04b3f12
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-02-06 12:37:50 +0530

    Add the Client Side Delay page & test file

---
+++ Assignment02/Pages/ClientSideDelayPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace CSE2522_Assignment02.Pages
+{
+    public class ClientSideDelayPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public ClientSideDelayPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://uitestingplayground.com/clientdelay");
+
+            
+            wait.Until(d => d.FindElement(By.Id("AJAXButton")));
+        }
+
+        public void ClickButton()
+        {
+            IWebElement button = wait.Until(d =>
+                d.FindElement(By.Id("AJAXButton"))
+            );
+
+            button.Click();
+        }
+
+        public string GetResult()
+        {
+            
+            WebDriverWait longWait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+
+            IWebElement result = longWait.Until(d =>
+                d.FindElement(By.XPath("//p[contains(text(),'Data calculated')]"))
+            );
+
+            return result.Text;
+        }
+    }
+}
---
+++ Assignment02/Tests/ClientSideDelayTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using CSE2522_Assignment02.Base;
+using CSE2522_Assignment02.Pages;
+
+namespace CSE2522_Assignment02.Tests
+{
+    public class ClientSideDelayTests : BaseTest
+    {
+        [Test]
+        public void VerifyContentLoadsAfterDelay()
+        {
+            var page = new ClientSideDelayPage(driver);
+
+            page.Open();        
+            page.ClickButton(); 
+
+            Assert.That(page.GetResult(), Does.Contain("Data calculated"));
+        }
+    }
+}
