ec4cf04e1ac0a0849c90463b43fd753d2a034c6a
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-02-06 12:39:20 +0530

    Add the Text input page & test file

---
+++ Assignment02/Pages/TextInputPage.cs
@@ -0,0 +1,45 @@
+﻿﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CSE2522_Assignment02.Pages
+{
+    public class TextInputPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public TextInputPage(IWebDriver driver, WebDriverWait wait)
+        {
+            this.driver = driver;
+            this.wait = wait;
+        }
+
+        IWebElement InputField =>
+            wait.Until(d => d.FindElement(By.Id("newButtonName")));
+
+        IWebElement Button =>
+            wait.Until(d => d.FindElement(By.Id("updatingButton")));
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://uitestingplayground.com/textinput");
+        }
+
+        public void EnterText(string text)
+        {
+            InputField.Clear();
+            InputField.SendKeys(text);
+        }
+
+        
+        public void UpdateButton()
+        {
+            Button.Click();
+        }
+
+        public string GetButtonText()
+        {
+            return Button.Text;
+        }
+    }
+}
---
+++ Assignment02/Tests/TextInputTests.cs
@@ -0,0 +1,23 @@
+﻿﻿using NUnit.Framework;
+using CSE2522_Assignment02.Base;
+using CSE2522_Assignment02.Pages;
+
+namespace CSE2522_Assignment02.Tests
+{
+    public class TextInputTests : BaseTest
+    {
+        [Test]
+        [Description("Update button text with valid input")]
+        public void UpdateButtonText_WithValidInput()'
+        {
+            var page = new TextInputPage(driver, wait);
+            page.Open();
+
+            page.EnterText("Test button");
+            page.UpdateButton(); 
+
+            Assert.That(page.GetButtonText(), Is.EqualTo("Test button"));
+        }
+    }
+}
+    
